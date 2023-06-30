84f0b26cce411ab4451dd8fa819e7e82eaf7441b
ztaccato <main.dev@ztaccato.com>
2023-06-30 17:49:11 -0400

    Added css support

--- global.d.ts
+++ global.d.ts
@@ -2,3 +2,8 @@ declare module '*.xml' {
     const content: string;
     export default content;
 }
+
+declare module '*.glsl' {
+    const content: string;
+    export default content;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,8 @@ module.exports = {
     module: {
         rules: [
             {test: /\.xml$/i, loader: 'raw-loader'},
-            {test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/}
+            {test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/},
+            {test: /\.css$/i, use: ["style-loader", "css-loader"]}
         ],
     },
 
